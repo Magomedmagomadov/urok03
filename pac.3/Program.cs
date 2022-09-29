fc5b16f2e4d253c4f0fbf34e3bdb2d14b9b4611d
Magomed Magomadov <magomed.magomadov_124@mail.ru>
2022-09-29 23:27:30 +0300

    Временно сделал наброску

---
+++ pac.3/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите число: ");
+int  n = Convert.ToInt32(Console.ReadLine());
+while (n >  0 && n < 10000)
+    Console.Write("Введите число: ");
+    if (n = n % 10);
+   
+else
+{
+    Console.WriteLine("нет");
+}
