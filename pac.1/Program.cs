52daa847da942c1a4a6f82573a73994eba45ed1d
Magomed Magomadov <magomed.magomadov_124@mail.ru>
2022-09-29 12:23:40 +0300

    Задача

---
+++ pac.1/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+Console.Write("Введите номер четверти: ");
+int x = Convert.ToInt32(Console.ReadLine());
+while (x < 1 || x  >4)
+     x = Convert.ToInt32(Console.ReadLine());
+
+if (x == 1)
+    Console.WriteLine("x > 0 and y > 0");
+else if (x == 2)
+    Console.WriteLine("x < 0 and y > 0");
+else if (x == 3)
+    Console.WriteLine("x < 0 and y < 0");
+ else if (x == 4)
+    Console.WriteLine("x > 0 and y < 0");
